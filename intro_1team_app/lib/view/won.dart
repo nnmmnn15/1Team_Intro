504b5b088943070bcda212671564282abe00d418
Leewonyoung <wylee99@naver.com>
2024-07-29 10:00:03 +0900

    Won_introduce

--- intro_1team_app/lib/view/won.dart
+++ intro_1team_app/lib/view/won.dart
@@ -1,10 +1,92 @@
 import 'package:flutter/material.dart';
 
-class Won extends StatelessWidget {
+class Won extends StatefulWidget {
   const Won({super.key});
 
   @override
+  State<Won> createState() => _WonState();
+}
+
+class _WonState extends State<Won> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/won_profile.jpg',
+                  width: 150,
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(8, 50, 8, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '이름',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
+                        child: Text(
+                          '이원영',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '나이',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
+                        child: Text(
+                          '만 25',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '거주지',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
+                        child: Text(
+                          '서울시 구로구',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '전공',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Text(
+                          '인공지능 소프트웨어',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
